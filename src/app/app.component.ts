c302b6ae3f3f3a13f1e6e381939c25676ac48b6a
esiomlink <simon.ecoffet@webinage.fr>
2024-06-07 10:27:32 +0200

    oui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,36 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'testMaterial';
+  test: string = '123';
+  myFonction(texte:string):string{
+    const simon :string = texte + '456'
+    return simon
+  }
+  oui = this.myFonction(this.test);
+// ---------------------------------
+ maVariable = 0;
+ incrementer():void { 
+  this.maVariable = this.maVariable+10;
+}
+//------------------------------------
+
+oui2: number[] = [1,2,3,4,5,6,7,8,9];
+
+myfc(): void{
+  for (let i=0; i< this.oui2.length ; i++){
+      console.log('i : ',i)
+      console.log('length : ',this.oui2.length)
+
+      console.log(this.oui2[i])
+  }
+}
+
+oui3=this.myfc()
+
+
+
+
+
+
+
 }
